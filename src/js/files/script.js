91760060da1aa003de5010f740be54fe5c549bd9
Lesha Light <senodin34@gmail.com>
2024-08-05 19:00:02 +0300

    rer

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,13 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+const parallaxElements = document.querySelectorAll('[data-parallax]');
+
+window.addEventListener('scroll', () => {
+  const y = window.scrollY;
+  parallaxElements.forEach((el) => {
+    const top = el.offsetTop;
+    el.style.transform = `translateY(-${ (y > top) ? y / 7 : 0 }px)`;
+  });
+});
